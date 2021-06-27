a667ee1775dc1b54c36af6aa26f347732d10bf34
Vishal Menon <vishalmenon.92@gmail.com>
2021-06-27 09:42:34 +0530

    Cleaned up exessive comments

--- src/cli.rs
+++ src/cli.rs
@@ -1,51 +1,9 @@
-// Working with subcommands is simple. There are a few key points to remember when working with
-// subcommands in clap. First, s are really just Apps. This means they can have their own
-// settings, version, authors, args, and even their own subcommands. The next thing to remember is
-// that subcommands are set up in a tree like hierarchy.
-//
-// An ASCII art depiction may about explain this better. Using a fictional version of git as the demo
-// subject. Imagine the following are all subcommands of git (note, the author is aware these aren't
-// actually all subcommands in the real git interface, but it makes explanation easier)
-//
-//            Top Level App (git)                         TOP
-//                           |
-//    -----------------------------------------
-//   /             |                \          \
-// clone          push              add       commit      LEVEL 1
-//   |           /    \            /    \       |
-//  url      origin   remote    ref    name   message     LEVEL 2
-//           /                  /\
-//        path            remote  local                   LEVEL 3
-//
-// Given the above fictional subcommand hierarchy, valid runtime uses would be (not an all inclusive
-// list):
-//
-// $ git clone url
-// $ git push origin path
-// $ git add ref local
-// $ git commit message
-//
-// Notice only one command per "level" may be used. You could not, for example, do:
-//
-// $ git clone url push origin path
-//
-// It's also important to know that subcommands each have their own set of matches and may have args
-// with the same name as other subcommands in a different part of the tree hierarchy (i.e. the arg
-// names aren't in a flat namespace).
-//
-// In order to use subcommands in clap, you only need to know which subcommand you're at in your
-// tree, and which args are defined on that subcommand.
-//
-// Let's make a quick program to illustrate. We'll be using the same example as above but for
-// brevity sake we won't implement all of the subcommands, only a few.
-
 use clap::{App, AppSettings, Arg};
 
 fn main() {
-    // this is where you define the top level app spec (currently called git)
     let matches = App::new("jcli")
         .about("A cli tool to interact with julie's database")
-        .version("1.0")
+        .version("0.0.3")
         .author("Stackmate.Network")
         .subcommand(
             App::new("info")
@@ -94,20 +52,14 @@ fn main() {
         .get_matches();
   
 
-    // This is where we handle each sub command
 
-    // `ArgMatches::subcommand` returns a tuple of both the name and matches
     match matches.subcommand() {
         ("info", Some(clone_matches)) => {
-            // Now we have a reference to clone's matches
             println!("Asked for info. Got none right now :(");
         }
-        // push is an example for handling nested subcommands
         ("client", Some(push_matches)) => {
-            // Now we have a reference to push's matches
             match push_matches.subcommand() {
                 ("register", Some(remote_matches)) => {
-                    // Now we have a reference to `push remote`'s matches
                     println!("'jcli client register' was called");
                 }
                 ("list", Some(_)) => {
@@ -121,10 +73,8 @@ fn main() {
         }
        
         ("service", Some(push_matches)) => {
-            // Now we have a reference to push's matches
             match push_matches.subcommand() {
                 ("register", Some(remote_matches)) => {
-                    // Now we have a reference to `push remote`'s matches
                     println!("'jcli service register' was called");
                 }
                 ("list", Some(_)) => {
@@ -137,8 +87,8 @@ fn main() {
             }
         }
        
-        ("",None) => println!("No subcommand was used"), // If no subcommand was used it'll match the tuple ("", None)
-        _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
+        ("",None) => println!("No subcommand was used"), 
+        _ => unreachable!(),
     }
 
 }
