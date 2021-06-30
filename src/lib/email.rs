d115e9abf5733c0fcf02e66572bf90d9a8938e94
Vishal Menon <vishalmenon.92@gmail.com>
2021-06-30 20:14:53 +0530

    Updated sendonlymail

--- src/lib/email.rs
+++ src/lib/email.rs
@@ -1,24 +1,34 @@
 extern crate lettre;
 extern crate lettre_email;
 
-use lettre::smtp::SmtpClient;
+use lettre::smtp::{SmtpClient};
 use lettre_email::EmailBuilder;
 use crate::lib::email::lettre::Transport;
 use lettre::Envelope;
 use lettre::EmailAddress;
 use lettre::SendableEmail;
 
+const JULIE_EMAIL: &str = "julie@satswala.com";
+
 pub fn sendmail(email: &str, alias: &str, message: &str)->bool{
     let to = EmailAddress::new(email.to_string()).unwrap();
-    let from = EmailAddress::new("julie@stackmate.net".to_string()).unwrap();
+    let from = EmailAddress::new(JULIE_EMAIL.to_string()).unwrap();
     let envelope = Envelope::new(Some(from),vec![to]).unwrap();
 
-    let email = SendableEmail::new(envelope,"ID".to_string(), message.as_bytes().to_vec());
-    
+    let email = SendableEmail::new(envelope,"MessageID".to_string(), message.as_bytes().to_vec());
+    // let email = EmailBuilder::new()
+    //     .to((email, alias))
+    //     .from(JULIE_EMAIL)
+    //     .subject("Woofy")
+    //     .text("supersecretokensauces")
+    //     .build()
+    //     .unwrap();
+
     let mut mailer = SmtpClient::new_unencrypted_localhost()
         .unwrap()
         .transport();
-    
+    // let mut mailer = SmtpTransport::builder_unencrypted_localhost().unwrap();
+                             
     let result = mailer.send(email);
     println!("{:#?}",result);
     result.is_ok()
@@ -31,7 +41,7 @@ mod tests {
 
     #[test]
     fn test_sendmail() {        
-        let email = "vishalmenon.92@gmail.com";
+        let email = "tech@stackmate.in";
         let alias = "Vishal Menon";
         let message = "supersecretauthtoken";
         assert!(sendmail(email, alias, message))
