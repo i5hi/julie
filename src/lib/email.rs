0fe598e3bdf6feab3fd0a09c88398a9217cdd7f2
Vishal Menon <vishalmenon.92@gmail.com>
2021-06-30 21:19:01 +0530

    Updated sendmail

--- src/lib/email.rs
+++ src/lib/email.rs
@@ -1,38 +1,50 @@
 extern crate lettre;
 extern crate lettre_email;
 
-use lettre::smtp::{SmtpClient};
-use lettre_email::EmailBuilder;
-use crate::lib::email::lettre::Transport;
-use lettre::Envelope;
-use lettre::EmailAddress;
-use lettre::SendableEmail;
+use lettre::{Message, Transport, SmtpTransport};
+// use crate::lib::email::lettre::Transport;
+// use lettre::Envelope;
+// use lettre::EmailAddress;
+// use lettre::SendableEmail;
 
-const JULIE_EMAIL: &str = "julie@satswala.com";
+use lettre_email::EmailBuilder;
+// use lettre::transport::Transport;
+
+const JULIE_EMAIL: &str = "admin@test.satswala.com";
 
 pub fn sendmail(email: &str, alias: &str, message: &str)->bool{
-    let to = EmailAddress::new(email.to_string()).unwrap();
-    let from = EmailAddress::new(JULIE_EMAIL.to_string()).unwrap();
-    let envelope = Envelope::new(Some(from),vec![to]).unwrap();
+    // let to = EmailAddress::new(email.to_string()).unwrap();
+    // let from = EmailAddress::new(JULIE_EMAIL.to_string()).unwrap();
+    // let envelope = Envelope::new(Some(from),vec![to]).unwrap();
 
-    let email = SendableEmail::new(envelope,"MessageID".to_string(), message.as_bytes().to_vec());
-    // let email = EmailBuilder::new()
-    //     .to((email, alias))
-    //     .from(JULIE_EMAIL)
-    //     .subject("Woofy")
-    //     .text("supersecretokensauces")
-    //     .build()
-    //     .unwrap();
+    // let email = SendableEmail::new(envelope,"MessageID".to_string(), message.as_bytes().to_vec());
+ 
 
-    let mut mailer = SmtpClient::new_unencrypted_localhost()
-        .unwrap()
-        .transport();
-    // let mut mailer = SmtpTransport::builder_unencrypted_localhost().unwrap();
+    // let mut mailer = SmtpClient::new_unencrypted_localhost()
+    //     .unwrap()
+    //     .transport();
+    // // let mut mailer = SmtpTransport::builder_unencrypted_localhost().unwrap();
                              
-    let result = mailer.send(email);
-    println!("{:#?}",result);
+    // let result = mailer.send(email.clone());
+    // println!("{:?}",email.message());
+    // result.is_ok()
+    let email = Message::builder()
+        .from(format!("Julie <{}>",JULIE_EMAIL).parse().unwrap())
+        .to(format!("{} <{}>",alias,email).parse().unwrap())
+        .subject("Woof Woof")
+        .body("Woof Woof".to_string())
+        .unwrap();
+
+    let transport = SmtpTransport::relay("smtp.gmail.com")
+        .unwrap()
+        .build();
+
+    let result = transport.send(&email);
     result.is_ok()
+    // true
+
 }
+
 
 
 #[cfg(test)]
