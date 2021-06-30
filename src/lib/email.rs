609dbfd1c212eb1665a28c8f2075e18a1d16491d
Vishal Menon <vishalmenon.92@gmail.com>
2021-06-30 21:53:02 +0530

    Working email

--- src/lib/email.rs
+++ src/lib/email.rs
@@ -1,45 +1,21 @@
 extern crate lettre;
-extern crate lettre_email;
 
 use lettre::{Message, Transport, SmtpTransport};
-// use crate::lib::email::lettre::Transport;
-// use lettre::Envelope;
-// use lettre::EmailAddress;
-// use lettre::SendableEmail;
-
-use lettre_email::EmailBuilder;
-// use lettre::transport::Transport;
 
 const JULIE_EMAIL: &str = "admin@test.satswala.com";
 
-pub fn sendmail(email: &str, alias: &str, message: &str)->bool{
-    // let to = EmailAddress::new(email.to_string()).unwrap();
-    // let from = EmailAddress::new(JULIE_EMAIL.to_string()).unwrap();
-    // let envelope = Envelope::new(Some(from),vec![to]).unwrap();
+pub fn sendmail(to: &str, alias: &str, message: &str)->bool{
 
-    // let email = SendableEmail::new(envelope,"MessageID".to_string(), message.as_bytes().to_vec());
- 
-
-    // let mut mailer = SmtpClient::new_unencrypted_localhost()
-    //     .unwrap()
-    //     .transport();
-    // // let mut mailer = SmtpTransport::builder_unencrypted_localhost().unwrap();
-                             
-    // let result = mailer.send(email.clone());
-    // println!("{:?}",email.message());
-    // result.is_ok()
-    let email = Message::builder()
+    let message = Message::builder()
         .from(format!("Julie <{}>",JULIE_EMAIL).parse().unwrap())
-        .to(format!("{} <{}>",alias,email).parse().unwrap())
+        .to(format!("{} <{}>",alias,to).parse().unwrap())
         .subject("Woof Woof")
-        .body("Woof Woof".to_string())
+        .body(message.to_string())
         .unwrap();
 
-    let transport = SmtpTransport::relay("smtp.gmail.com")
-        .unwrap()
-        .build();
+    let transport = SmtpTransport::unencrypted_localhost();
 
-    let result = transport.send(&email);
+    let result = transport.send(&message);
     result.is_ok()
     // true
 
@@ -53,10 +29,10 @@ mod tests {
 
     #[test]
     fn test_sendmail() {        
-        let email = "tech@stackmate.in";
-        let alias = "Vishal Menon";
-        let message = "supersecretauthtoken";
-        assert!(sendmail(email, alias, message))
+        let to = "vishalmenon.92@gmail.com";
+        let alias = "vmd";
+        let message = "https://test.satswala.com/julie?token=supermostsecrettokenforyoumyfriendlyboi";
+        assert!(sendmail(to, alias, message))
     }
 
 }
